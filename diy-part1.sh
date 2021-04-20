bde7f888de1ad45aab2cb4f03f2620c036fe46ba
Vivian Westwood <75788133+VivianWestwood@users.noreply.github.com>
2021-04-20 18:11:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,7 @@
 #
 
 # Uncomment a feed source 取消SSR-PLus注释
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source 额外添加feed源
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
